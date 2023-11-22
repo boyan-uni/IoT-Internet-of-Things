2fbc1377ceec010c032cae5c83576a4dc4c8e22d
Boyan <bupt_lby2016@163.com>
2023-11-22 18:14:19 +0000

    Update task_3.py with initial cde code.

--- task_3.py
+++ task_3.py
@@ -10,17 +10,19 @@ rabbitmq_username = "guest"
 rabbitmq_password = "guest" 
 
 
+# Collect data from RabbitMQ
 def collect_data_from_rabbitmq():
     try:
         credentials = pika.PlainCredentials(rabbitmq_username, rabbitmq_password)
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=rabbitmq_ip, port=rabbitmq_port, credentials=credentials))
         channel = connection.channel()
-
         channel.queue_declare(queue=rabbitmq_queue)
 
+        data_list = []    # store data before sorting by timestamp
+        data_result = []  # store final data format
+        
         print("Collecting Daily Average PM2.5 Data:")
-        data_list = []  # store data before sorting by timestamp
         for method_frame, properties, body in channel.consume(queue=rabbitmq_queue, auto_ack=True):
             data_rmq = json.loads(body)
             data_list.append(data_rmq)
@@ -28,13 +30,81 @@ def collect_data_from_rabbitmq():
             timestamp_unix = int(data_rmq['Timestamp'])//1000
             timestamp = datetime.utcfromtimestamp(timestamp_unix).strftime('%Y-%m-%d %H:%M:%S')
             print(f"Timestamp: {timestamp}, Value: {data_rmq['Average_Value']}")
-
+            data_result.append({'Timestamp': timestamp, 'Value': data_rmq['Average_Value']})
+            
         # close connection
         channel.cancel()
         connection.close()
+        return data_result
     except Exception as e:
         print(f"Error Message: {e}")
 
 
+# Visualize daily average PM2.5 data
+def visualize_daily_average(data):
+    data_df = pd.DataFrame(data)
+    
+    # Initialize a canvas
+    plt.figure(figsize=(8, 4), dpi=200)
+
+    # Plot data into canvas
+    plt.plot(data_df["Timestamp"], data_df["Value"], color="#FF3B1D", marker='.', linestyle="-")
+    plt.title('Daily Average PM2.5 Data')
+    plt.xlabel('DateTime')
+    plt.ylabel('Value')
+
+    plt.legend()
+    plt.savefig('daily_average_pm25.png')
+    plt.show()
+
+
+# Predict PM2.5 using machine learning
+def predict_pm25(data):
+    # 将数据转换为DataFrame
+    data_df = pd.DataFrame(data)
+
+    # Create ML engine predictor object
+    predictor = MLPredictor(data_df)
+    # Train ML model
+    predictor.train()
+    # Do prediction
+    forecast = predictor.predict()
+
+	# Get canvas
+    fig = predictor.plot_result(forecast)
+    fig.savefig("prediction.png")
+    fig.show()
+    return forecast
+
+
+# Visualize prediction results
+def visualize_forecast(data):
+    data_df = pd.DataFrame(data)
+    
+    # Initialize a canvas
+    plt.figure(figsize=(8, 4), dpi=200)
+
+    # Plot data into canvas
+    plt.plot(data_df["Timestamp"], data_df["Value"], color="#FF3B1D", marker='.', linestyle="-")
+    plt.title('PM2.5 Data Prediction')
+    plt.xlabel('DateTime')
+    plt.ylabel('Value')
+
+    plt.legend()
+    plt.savefig('pm25_prediction.png')
+    plt.show()
+
+
 if __name__ == '__main__':
-    collect_data_from_rabbitmq()
+    
+    # Collect data from RabbitMQ
+    data_result = collect_data_from_rabbitmq()
+
+    # Visualize daily average PM2.5 data
+    visualize_daily_average(data_result)
+
+    # Predict PM2.5 using machine learning
+    # forecast_result = predict_pm25(data_result)
+
+    # Visualize prediction results
+    # visualize_forecast(forecast_result)
